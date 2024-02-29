bb2ed0a90c9d9e87b5e3aafbbff1c3e808a70101
Алексей Гильдебрант <mashinis@gmail.com>
2024-02-29 16:18:05 +0800

    провел рефакторинг и удалил лишние классы

--- src/main/java/ru/mashinis/dao/interfaces/dell/FormFieldsDao.java
+++
@@ -1,7 +0,0 @@
-package ru.mashinis.dao.interfaces.dell;
-
-import java.util.List;
-
-public interface FormFieldsDao<T> {
-    List<T> getByFormId(int id);
-}
--- src/main/java/ru/mashinis/exception/UserRegistrationException.java
+++
@@ -1,7 +0,0 @@
-package ru.mashinis.exception;
-
-public class UserRegistrationException extends RuntimeException {
-    public UserRegistrationException(String message) {
-        super(message);
-    }
-}
